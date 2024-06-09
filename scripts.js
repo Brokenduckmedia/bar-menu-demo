3462803f4e822117992b9708d2d6779a024a53a9
Brokenduckmedia <brokenduckmedia@gmail.com>
2024-06-09 16:27:25 -0400

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -1,4 +1,14 @@
+
 function orderDrink(drink, table) {
-    alert(`Order placed for ${drink} from ${table}`);
-    // This is a placeholder. You can add code here to send the order to a backend service or Google Sheets.
+    var scriptURL = 'https://script.google.com/macros/s/AKfycbxfpZR5NAIt-wvbTW85pI6hge6PR8Pz4j9XzK4LZ_ukyhEjTg8_j3yGFSIUKuUh4dGWeQ/exec';
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', scriptURL + '?drink=' + encodeURIComponent(drink) + '&table=' + encodeURIComponent(table));
+    xhr.onload = function() {
+        if (xhr.status === 200) {
+            alert('Order placed for ' + drink + ' from ' + table);
+        } else {
+            alert('Failed to place order. Please try again.');
+        }
+    };
+    xhr.send();
 }
