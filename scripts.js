ac22b48517f5a1720b7c924816d2cc4245a7ff68
Brokenduckmedia <brokenduckmedia@gmail.com>
2024-06-09 15:53:26 -0400

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,4 @@
+function orderDrink(drink, table) {
+    alert(`Order placed for ${drink} from ${table}`);
+    // This is a placeholder. You can add code here to send the order to a backend service or Google Sheets.
+}
